20411b5f1d720e3f13a2c5795e4b7aaaee29cba5
oopadira <31675008+oopadira@users.noreply.github.com>
2017-10-08 16:23:35 -0700

    jhhbJ

---
+++ Timesheet/Controllers/TimesheetReportsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimesheetReports.Controllers
+{
+    public class TimesheetReportsController : Controller
+    {
+        // GET: TimesheetReports
+        public ActionResult TimesheetReports()
+        {
+            return View();
+        }
+    }
+}
