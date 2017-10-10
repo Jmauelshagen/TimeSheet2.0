1211af140ad0e7c5155ec0869def90ece8d9c90e
Scott Lilly <dragonfly31721@yahoo.com>
2017-10-10 16:09:04 -0400

    d

--- Timesheet/Controllers/TimesheetController.cs
+++ Timesheet/Controllers/TimesheetController.cs
@@ -63,5 +63,34 @@ namespace Timesheet.Controllers
 
             return RedirectToAction("GetTimesheet", "Timesheet");
         }
+        [HttpPost]
+        public void Time(Models.TimeSheet model)
+        {
+            //Pull the employee object from the session.
+            Employee emp = (Employee)Session["Employee"];
+
+            //Instantiate TimeSheet object with data from form
+            TimeSheet sheet = new TimeSheet
+            {
+                Id = model.Id,
+                WeekEnding = model.WeekEnding,
+                Date = model.Date,
+                TimeIn = model.TimeIn,
+                OutForLunch = model.OutForLunch,
+                InFromLunch = model.InFromLunch,
+                TimeOut = model.TimeOut,
+                LeaveId = model.LeaveId,
+                LeaveHours = model.LeaveHours,
+                TotalHoursWorked = model.TotalHoursWorked,
+                Submitted = model.Submitted,
+                AuthorizedBySupervisor = model.AuthorizedBySupervisor,
+                EmpId = model.EmpId
+            };
+
+            sheet.InsertTimeSheet(sheet);
+
+            RedirectToAction("GetTimesheet", "Timesheet");
+        }
+
     }
 }
