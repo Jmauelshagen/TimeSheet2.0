8e00bfe97b951182e63d3de6ba8ab5763d8737c3
Greg-D-Robertson <greg_robertson@yahoo.com>
2017-10-10 21:04:08 -0400

    Trying to get the timesheeet to work

--- Timesheet/Models/Employee.cs
+++ Timesheet/Models/Employee.cs
@@ -12,7 +12,7 @@ namespace Timesheet.Models
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    
+
     public partial class Employee
     {
         //Instance variables
