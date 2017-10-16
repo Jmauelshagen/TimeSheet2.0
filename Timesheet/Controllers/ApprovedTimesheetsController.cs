a5acf879f2f4c4a54acbf42567d0fd3659eed4b4
oopadira <31675008+oopadira@users.noreply.github.com>
2017-10-16 08:38:36 -0700

    report
    reports

---
+++ Timesheet/Controllers/ApprovedTimesheetsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Timesheet.Controllers
+{
+    public class ApprovedTimesheetsController : Controller
+    {
+        // GET: ApprovedTimesheets
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
