fac51b7538b22cc23fc35e8b9b6449bf7fe31ddf
JessaAnne <31674456+JessaAnne@users.noreply.github.com>
2017-11-02 20:23:43 -0400

    11.2 unit testing added to my branch

---
+++ TimesheetTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TimesheetTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TimesheetTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b90959f9-47b9-4b02-8c04-209fe2c1caf6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TimesheetTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Helpers;
+using Timesheet.Controllers;
+using TimesheetReports.Controllers;
+using System.Web.Mvc;
+
+namespace TimesheetTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Arrange
+            TimesheetController controller = new TimesheetController();
+            //Act
+            ViewResult result = controller.Timesheet() as ViewResult;
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
