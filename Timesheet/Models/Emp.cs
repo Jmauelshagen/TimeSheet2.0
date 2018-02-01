1b1cf9285ef760fc24d6c223f98807f4b5e691d3
rspeight <rspeight@students.chattahoocheetech.edu>
2018-02-01 01:49:54 -0500

    test: changing gitignore

--- Timesheet/Models/Emp.cs
+++ Timesheet/Models/Emp.cs
@@ -17,6 +17,6 @@ namespace Timesheet.Models
         public int EmpId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        //This is a test!
+        //This is a test
     }
 }
