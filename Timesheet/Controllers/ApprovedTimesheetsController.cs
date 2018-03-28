5a00a9d34ae7968ee9780132d70b208843ce0026
rspeight <rspeight@students.chattahoocheetech.edu>
2018-03-27 20:25:41 -0400

    Approved Timesheet included ID

--- Timesheet/Controllers/ApprovedTimesheetsController.cs
+++ Timesheet/Controllers/ApprovedTimesheetsController.cs
@@ -14,16 +14,20 @@ namespace ApprovedTimesheets.Controllers
         public ActionResult ApprovedTimesheets()
         {
             LoginDatabaseEntities1 db = new LoginDatabaseEntities1();
-
-            ViewBag.Employees = new SelectList(db.Employees, "Banner_ID", "First_Name");            
-            return View();
+            var sup = (Employee)Session["Supervisor"];
+            ViewBag.Employees = new SelectList(db.Employees, "Banner_ID", "First_Name");
+            var model = new TimeSheet
+            {
+                EmpNames = GetEmployeeNames(sup.Banner_ID),
+                
+            };
+            return View(model);
         }
 
         [HttpPost]
         public ActionResult ApprovedData(TimeSheet model)
-        {
-            int emp = Int32.Parse(Request.Form["Employee"].ToString());
-            Debug.WriteLine("Emp value is : " + emp + " ]");
+        {            
+            int emp = Convert.ToInt32(model.Name.Trim());            
             Employee emp1 = new Employee();
             Session["Employee"] = emp1.GetEmployee(emp);
             TimeSheet timesheet = new TimeSheet();           
@@ -36,6 +40,24 @@ namespace ApprovedTimesheets.Controllers
             Debug.WriteLine("The size of approveList is: " + approveList.Count);
             Session["AppTimeSheetData"] = approveList;
             return RedirectToAction("ApprovedTimesheets", "ApprovedTimesheets");
+        }
+
+
+        //Obtains a list of employee names from the db and adds them to a select list
+        //to be used in the UI as a menu
+        private IEnumerable<SelectListItem> GetEmployeeNames(int sid)
+        {
+            TimeSheet timeSheet = new TimeSheet();
+            var namesList = new List<SelectListItem>();
+            foreach (string names in timeSheet.GetEmployeeNames(sid))
+            {
+                namesList.Add(new SelectListItem
+                {
+                    Value = names.Split(',')[1].Trim(),
+                    Text = names
+                });
+            }
+            return namesList;
         }
     }
 }
